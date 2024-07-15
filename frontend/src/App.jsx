ca43fe4d223d65a73215ea041720b48f0eef34b2
nisaacdz <nisaacdz@gmail.com>
2024-07-15 04:50:53 +0000

    implemented basic layout and information passing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,47 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import Board from './board/Board';
 
 function App() {
-  // const [greetMsg, setGreetMsg] = useState("");
-  // const [name, setName] = useState("");
+  const boardRef = useRef(null);
+  const activeMouseRef = useRef(null);
+
+  const mouseTouchBoard = () => {
+    if (boardRef.current != null) {
+      // handle on click on this point
+    }
+  }
+  
+  const mouseMoveHandler = (x, y) => {
+    if (activeMouseRef.current != null && activeMouseRef.current != {x: x, y: y}) {
+      activeMouseRef.current = {x: x, y: y};
+      mouseTouchBoard();
+    }
+  };
+
+  const mouseDownHandler = (x, y) => {
+    activeMouseRef.current = {x: -1, y: -1};
+    mouseMoveHandler(x, y);
+  };
+  
+  const mouseUpHandler = () => {
+    activeMouseRef.current = null;
+  };
+  
+  const mouseLeaveHandler = () => {
+    activeMouseRef.current = null;
+  };
 
   return (
     <div id="content">
-      <Board />
+      <Board 
+        mouseDownHandler={mouseDownHandler} 
+        mouseMoveHandler={mouseMoveHandler} 
+        mouseUpHandler={mouseUpHandler} 
+        mouseLeaveHandler={mouseLeaveHandler}
+        ref={boardRef}
+      />
     </div>
   );
 }
--- frontend/src/board/Board.jsx
+++ frontend/src/board/Board.jsx
@@ -1,29 +1,60 @@
 import React, { useImperativeHandle } from 'react';
 import './Board.css';
 
-function Board() {
+function Board({ mouseDownHandler, mouseMoveHandler, mouseUpHandler, mouseLeaveHandler }) {
     const canvasRef = useRef(null);
 
     useImperativeHandle(ref, () => ({
-        changePixelColor: (x, y, color) => {
+        setPixelColor: (x, y, color) => {
             const ctx = canvasRef.current.getContext('2d');
             ctx.fillStyle = color;
             ctx.fillRect(x, y, 1, 1);
+        },
+        getPixelColor: (x, y) => {
+            const ctx = canvasRef.current.getContext('2d');
+            return ctx.getPixelColor(x, y)
         },
         moveScreen: (xtrans, ytrans) => {
 
         }
     }));
 
-    const handleClick = (event) => {
-        // Get the coordinates of the click
+    const handleMouseDown = (event) => {
         const rect = event.target.getBoundingClientRect();
         const x = event.clientX - rect.left;
         const y = event.clientY - rect.top;
+        mouseDownHandler(x, y)
+    };
+
+    const handleMouseMove = (event) => {
+        const rect = event.target.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        mouseMoveHandler(x, y)
+    };
+
+    const handleMouseUp = (event) => {
+        const rect = event.target.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        mouseUpHandler(x, y)
+    };
+
+    const handleMouseLeave = (event) => {
+        const rect = event.target.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
+        mouseLeaveHandler(x, y)
     };
 
     return (
-        <div id="board" onClick={handleClick} ref={canvasRef}/>
+        <div id="board"
+        onClick={handleClick}
+        ref={canvasRef}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseLeave} />
     );
 };
 
