a04a70b477ac6daa201a0f571c675cfab65f7551
nisaacdz <nisaacdz@gmail.com>
2024-08-09 07:37:50 +0000

    ddsldjsdsddjk

--- frontend/src/board/Board.jsx
+++ frontend/src/board/Board.jsx
@@ -96,24 +96,6 @@ function cubicBezierSpline(p0, p1, p2, p3, t) {
   return { x, y };
 }
 
-/*
-void bezierCurve(int x[] , int y[])
-{
-    double xu = 0.0 , yu = 0.0 , u = 0.0 ;
-    int i = 0 ;
-    for(u = 0.0 ; u <= 1.0 ; u += 0.0001)
-    {
-        xu = pow(1-u,3)*x[0]+3*u*pow(1-u,2)*x[1]+3*pow(u,2)*(1-u)*x[2]
-             +pow(u,3)*x[3];
-        yu = pow(1-u,3)*y[0]+3*u*pow(1-u,2)*y[1]+3*pow(u,2)*(1-u)*y[2]
-            +pow(u,3)*y[3];
-        SDL_RenderDrawPoint(renderer , (int)xu , (int)yu) ;
-    }
-}
-
-Use the bove implementation (but fit it for my use case). It seems they made use of 3 points on the 
-*/
-
 function catmullRomSpline(p0, p1, p2, p3, t) {
   let t2 = t * t;
   let t3 = t2 * t;
